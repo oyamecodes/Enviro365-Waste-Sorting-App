c18ee36d2d9d82041034819acc13881b4d3f2636
oyame <jameskingk853@gmail.com>
2025-02-16 16:58:50 +0200

    project setup

---
+++ enviro365-waste-sorting/src/main/java/com/enviro/assessment/grad001/OyameMazaleni/WasteManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.enviro.assessment.grad001.OyameMazaleni;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WasteManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WasteManagementSystemApplication.class, args);
+	}
+
+}
---
+++ enviro365-waste-sorting/src/test/java/com/enviro/assessment/grad001/OyameMazaleni/WasteManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enviro.assessment.grad001.OyameMazaleni;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WasteManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
